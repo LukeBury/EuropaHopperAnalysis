e81ced8e00090d274df57d9d7119a16442760927
Luke Bury <lukebury@Luke-Burys-MacBook-Pro.local>
2017-01-17 14:28:59 -0700

    Broke up hopper dynamics section into seperate jupiter and europa forces

--- ProjectBin/EH_NumIntegrator_CR3BP.m
+++ ProjectBin/EH_NumIntegrator_CR3BP.m
@@ -1,7 +1,7 @@
 % ------------------------------------------------------------------------
 %%% Europa-and-Jupiter Numerical Integrator
 % ------------------------------------------------------------------------
-function [ dY ] = EJ_EOMIntegrator_CR3BP(t,Y,E_radius,uE,uJ,nE,E_a)
+function [ dY ] = EH_NumIntegrator_CR3BP(t,Y,E_radius,uE,uJ,nE,E_a)
 dY = zeros(6,1);
 
 %%% Creating Europa Position (JCI)
@@ -17,7 +17,9 @@ dyH = Y(4:6);
 rH = yH - rE; % km
 
 %%% Hopper Dynamics
-ddyH = (-uJ/(norm(yH)^3))*yH + (-uE/(norm(rH)^3))*rH; % Jupiter and Europa pull km/s^2
+fJ = (-uJ/(norm(yH)^3))*yH;
+fE = (-uE/(norm(rH)^3))*rH;
+ddyH = fJ + fE; % Jupiter and Europa pull km/s^2
 
 %%% Output the derivative of the state
 dY(1:3) = dyH;
