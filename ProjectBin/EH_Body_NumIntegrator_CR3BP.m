44f36fd4a8671c2887a99d843ff184fe2d56c408
Luke Bury <lukebury@Luke-Burys-MacBook-Pro.local>
2017-02-03 15:30:54 -0700

    Deleted lots of unnecessary comments

--- ProjectBin/EH_Body_NumIntegrator_CR3BP.m
+++ ProjectBin/EH_Body_NumIntegrator_CR3BP.m
@@ -3,23 +3,6 @@
 % ------------------------------------------------------------------------
 function [ dY ] = EH_NumIntegrator_CR3BP(t,Y,E_radius,uE,uJ,nE,E_a)
 dY = zeros(6,1);
-% 
-% %%% Creating Europa Position (JCI)
-% ta = nE*t; % rads
-% rE = [E_a; 0; 0]; % km
-% rE = R3(rE,ta); % km
-% 
-% %%% Unpack the Hopper state vector (JCI)
-% yH = Y(1:3); % Hopper Position, km
-% dyH = Y(4:6); % Hopper Velocity, km/s
-% 
-% %%% Europa-Centric hopper position (ECI)
-% rH = yH - rE; % km
-% 
-% %%% Hopper Dynamics
-% fJ = (-uJ/(norm(yH)^3))*yH; % Jupiter Pull, km/s^2
-% fE = (-uE/(norm(rH)^3))*rH; % Europa Pull, km/s^2
-% ddyH = fJ + fE; % Jupiter and Europa pull km/s^2
 
 %%% Unpack the Hopper state vector (ECEF)
 yH = Y(1:3); % Hopper Position, km
