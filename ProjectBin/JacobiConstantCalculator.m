3eda3d5cb91470e38d416bac869d42f599812649
Luke Bury <lukebury@Luke-Burys-MacBook-Pro.local>
2017-03-20 11:51:05 -0600

    Simplified code to return Jacobi constants

--- ProjectBin/JacobiConstantCalculator.m
+++ ProjectBin/JacobiConstantCalculator.m
@@ -9,16 +9,17 @@
 % 5) Position of secondary body in BCR frame .....[1x3]
 %%% Outputs:
 % 1) Jacobi constants ............................[nx1]
-function [JCs] = JacobiConstantCalculator(u,rH,vH,rB1,rB2)
+function [JCs] = JacobiConstantCalculator(u,rH,vH)
+if size(rH) ~= size(vH)
+    warning('Mismatch of input dimensions')
+    return
+end
+rB1 = [-u, 0, 0];
+rB2 = [1-u, 0, 0];
 JCs = zeros(length(rH),1);
-dimTest = size(rH,2) + size(vH,2) + size(rB1,2) + size(rB2,2);
-if dimTest == 12
-    for k = 1:length(rH)
-        r1 = norm(rB1 - rH(k,:));
-        r2 = norm(rB2 - rH(k,:));
-        JCs(k) = rH(k,1)^2 + rH(k,2)^2 + 2*(1-u)/r1 + 2*u/r2 - vH(k,1)^2 - vH(k,2)^2 - vH(k,3)^2;
-    end
-else
-    warning('Check input dimensions\n')
+for k = 1:length(rH)
+    r1 = norm(rB1 - rH(k,:));
+    r2 = norm(rB2 - rH(k,:));
+    JCs(k) = rH(k,1)^2 + rH(k,2)^2 + 2*(1-u)/r1 + 2*u/r2 - vH(k,1)^2 - vH(k,2)^2 - vH(k,3)^2;
 end
 end
